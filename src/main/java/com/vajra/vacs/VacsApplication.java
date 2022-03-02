051a5e7bb0b0bd9401086385a25c2dca33021bed
applock <ibsanchar@gmail.com>
2022-03-02 09:00:09 +0530

    init

---
+++ src/main/java/com/vajra/vacs/VacsApplication.java
@@ -0,0 +1,13 @@
+package com.vajra.vacs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vajra/vacs/VacsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vajra.vacs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
